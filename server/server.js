79ca1fa840a60a6fe69df1ee61d0e8d9dc426f8b
bharatbhaichaudhary <bharatch6353@gmail.com>
2025-01-28 18:43:32 +0530

    minor update

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const userRoute = require("./routes/user.rout");
 const blogRouter = require("./routes/blog.route");
 const conectDb = require("./utils/db");
+const Blog = require("./models/blog.model");
 
 const app = express();
 
@@ -22,8 +23,14 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.send("dskjc/api");
 });
-app.get("/blog", (req, res) => {
-  res.send("dskjc/api");
+app.get("/blog", async (req, res) => {
+  try {
+    const blog = await Blog.find();
+
+    res.status(200).json({ success: true, blog });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 });
 app.use("/api/user", userRoute);
 app.use("/api/blog", blogRouter);
