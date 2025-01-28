1d5534ab6f87bff5766e49d126dc49d22b9cef7f
bharatbhaichaudhary <bharatch6353@gmail.com>
2025-01-28 19:23:04 +0530

    minor update env

--- server/server.js
+++ server/server.js
@@ -16,23 +16,6 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
-
-app.get("/", (req, res) => {
-  res.send("dskjc");
-});
-app.get("/api", (req, res) => {
-  res.send("dskjc/api");
-});
-app.get("/blog", async (req, res) => {
-  try {
-    
-    const blog = await Blog.find();
-
-    res.status(200).json({ success: true, blog });
-  } catch (error) {
-    res.status(400).json({ success: false, message: error.message });
-  }
-});
 app.use("/api/user", userRoute);
 app.use("/api/blog", blogRouter);
 
